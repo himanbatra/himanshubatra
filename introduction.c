05d8b1e5c4d444401b7ccba9a50f46f90d46f7ba
Sam Smith <himanshubatra7512@gmail.com>
2021-12-12 12:09:43 +0530

    him

---
+++ introduction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+printf("name=himanshu batra");
+printf("gender=male");
+printf("branch=information technology");
+printf("state=rajasthan ");
+printf("hobbie=badmintion");
+return 0;
+}
